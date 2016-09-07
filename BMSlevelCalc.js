d9bf9f55add8795039867eca8383ebcd2a7de511
TomoAlien <tomoalien@o2.pl>
2016-09-07 23:13:13 +0200

    Improved Formula
    Improved formula as suggested by JerwuQu
    -Generalized base function
    -Derivative function with weighting parameters optimized for 5-key charts.

--- BMSlevelCalc.js
+++ BMSlevelCalc.js
@@ -1,4 +1,9 @@
-function BMSlevelCalc(notes,scratch,length){
-level = Math.round((((notes*2)+(scratch*3)/3)/(length))*(2/3));
+function BMSlevelCalc(notes,notes_w,scratch,scratch_w,length,multiplier){
+level = Math.round((notes*notes_w+scratch*scratch_w)/length*multiplier);
 return level;
+}
+
+function FiveLevelCalc(notes,scratch,length){
+multiplier = 4/7;
+return BMSlevelCalc(notes,2,scratch,3,length,multiplier);
 }
