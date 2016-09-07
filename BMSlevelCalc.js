24b328d2c742ee77c082e27975376f1befa3c1a2
TomoAlien <tomoalien@o2.pl>
2016-09-07 23:27:18 +0200

    Small optimization

--- BMSlevelCalc.js
+++ BMSlevelCalc.js
@@ -4,6 +4,5 @@ return level;
 }
 
 function FiveLevelCalc(notes,scratch,length){
-multiplier = 4/7;
-return BMSlevelCalc(notes,2,scratch,3,length,multiplier);
+return BMSlevelCalc(notes,2,scratch,3,length,(4/7));
 }
